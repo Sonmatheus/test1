080aaf20a7495495a816c1e500ff4d0a095caad3
Matheus Torres <132587437+Sonmatheus@users.noreply.github.com>
2026-06-04 16:13:43 -0300

    js.js

---
+++ js.js
@@ -0,0 +1,14 @@
+let nota1 = 8;
+
+if (nota1 >= 7) {
+    console.log("Voçê foi aprovado.")
+} 
+if (nota1 >= 5 |) {
+    console.log("Voçê está de recuperação.")
+
+} else if (nota1 <= 4) {
+    console.log("Voçê está reprovado")
+} 
+
+
+console.log(nota1);
